d7c1ef568144814158319bbf4ffc52228d9e44b6
Rafael Calafell Cladera <rafaelillopalma@hotmail.com>
2019-03-06 13:15:12 +0100

    Corrección

---
+++ SearchData/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SearchData
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblDelimiter = new System.Windows.Forms.Label();
+            this.txtDelimiter = new System.Windows.Forms.TextBox();
+            this.cbHeader = new System.Windows.Forms.CheckBox();
+            this.gbStep2 = new System.Windows.Forms.GroupBox();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.gbStep2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(339, 18);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(107, 23);
+            this.btnLoadFile.TabIndex = 3;
+            this.btnLoadFile.Text = "Load File";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblDelimiter);
+            this.groupBox1.Controls.Add(this.txtDelimiter);
+            this.groupBox1.Controls.Add(this.cbHeader);
+            this.groupBox1.Controls.Add(this.btnLoadFile);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(452, 51);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Step 1";
+            // 
+            // lblDelimiter
+            // 
+            this.lblDelimiter.AutoSize = true;
+            this.lblDelimiter.Location = new System.Drawing.Point(190, 23);
+            this.lblDelimiter.Name = "lblDelimiter";
+            this.lblDelimiter.Size = new System.Drawing.Size(50, 13);
+            this.lblDelimiter.TabIndex = 6;
+            this.lblDelimiter.Text = "Delimiter:";
+            // 
+            // txtDelimiter
+            // 
+            this.txtDelimiter.Location = new System.Drawing.Point(246, 20);
+            this.txtDelimiter.MaxLength = 1;
+            this.txtDelimiter.Name = "txtDelimiter";
+            this.txtDelimiter.Size = new System.Drawing.Size(26, 20);
+            this.txtDelimiter.TabIndex = 2;
+            this.txtDelimiter.Text = "|";
+            this.txtDelimiter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cbHeader
+            // 
+            this.cbHeader.AutoSize = true;
+            this.cbHeader.Checked = true;
+            this.cbHeader.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbHeader.Location = new System.Drawing.Point(44, 22);
+            this.cbHeader.Name = "cbHeader";
+            this.cbHeader.Size = new System.Drawing.Size(67, 17);
+            this.cbHeader.TabIndex = 1;
+            this.cbHeader.Text = "Header?";
+            this.cbHeader.UseVisualStyleBackColor = true;
+            // 
+            // gbStep2
+            // 
+            this.gbStep2.Controls.Add(this.dgvData);
+            this.gbStep2.Controls.Add(this.btnSearch);
+            this.gbStep2.Controls.Add(this.txtSearch);
+            this.gbStep2.Enabled = false;
+            this.gbStep2.Location = new System.Drawing.Point(12, 69);
+            this.gbStep2.Name = "gbStep2";
+            this.gbStep2.Size = new System.Drawing.Size(452, 369);
+            this.gbStep2.TabIndex = 3;
+            this.gbStep2.TabStop = false;
+            this.gbStep2.Text = "Step 2";
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(6, 45);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.Size = new System.Drawing.Size(440, 318);
+            this.dgvData.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(339, 17);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(107, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(6, 19);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(327, 20);
+            this.txtSearch.TabIndex = 5;
+            this.txtSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSearch_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 450);
+            this.Controls.Add(this.gbStep2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(492, 489);
+            this.MinimumSize = new System.Drawing.Size(492, 489);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Agenda telefónica";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.gbStep2.ResumeLayout(false);
+            this.gbStep2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gbStep2;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.CheckBox cbHeader;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Label lblDelimiter;
+        private System.Windows.Forms.TextBox txtDelimiter;
+    }
+}
+
---
+++ SearchData/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchData
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            // Clear DataSource
+            dgvData.DataSource = null;
+
+            // Open file dialog
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Comma Separated Value (*.csv)|*.csv";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // schema.ini
+                    generateSchema(openFileDialog.FileName);
+
+                    // Read CSV
+                    DataTable dt = readCSV(openFileDialog.FileName);
+
+                    // Only with rows and 3 columns(min.)
+                    if (dt.Rows.Count > 0 && dt.Columns.Count > 2)
+                    {
+                        // Rename columns
+                        try
+                        {
+                            dt.Columns[0].ColumnName = "Name";
+                            dt.Columns[1].ColumnName = "City";
+                            dt.Columns[2].ColumnName = "Phone";
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+
+                        // Add the DataSource
+                        dgvData.DataSource = dt;
+                        // Enable Step 2
+                        gbStep2.Enabled = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("No rows found to list or did not contain 3 columns");
+                        gbStep2.Enabled = false;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Generate schema.ini
+        /// </summary>
+        /// <param name="filePath">File path of CSV</param>
+        private void generateSchema(String filePath)
+        {
+            String path = Path.Combine(Path.GetDirectoryName(filePath), "schema.ini");
+
+            // Create text of schema.ini
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                // CSV file
+                sw.WriteLine("["+Path.GetFileName(filePath)+"]");
+                // Delimited
+                sw.WriteLine("Format=Delimited("+this.txtDelimiter.Text+")");
+                // Header?
+                sw.WriteLine("ColNameHeader=" + (this.cbHeader.Checked ? "True" : "False"));
+            }
+        }
+
+        /// <summary>
+        /// Read CSV from file
+        /// </summary>
+        /// <param name="filePath">Path of specified file</param>
+        /// <returns>DataTable of CSV</returns>
+        private DataTable readCSV(String filePath)
+        {
+            // Vars
+            string pathOnly = Path.GetDirectoryName(filePath);
+            string fileName = Path.GetFileName(filePath);
+            // Header?
+            string header = this.cbHeader.Checked ? "Yes" : "No";
+            // Query
+            string sql = @"SELECT * FROM [" + fileName + "]";
+
+            // Connection
+            using (OleDbConnection connection = new OleDbConnection(
+                      @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathOnly +
+                      ";Extended Properties='text;HDR=" + header + ";'"))
+            using (OleDbCommand command = new OleDbCommand(sql, connection))
+            using (OleDbDataAdapter adapter = new OleDbDataAdapter(command))
+            {
+                // Generate DataTable
+                DataTable dataTable = new DataTable();
+                dataTable.Locale = CultureInfo.CurrentCulture;
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Search by Name and City
+                (dgvData.DataSource as DataTable).DefaultView.RowFilter = String.IsNullOrEmpty(txtSearch.Text) ?
+                    "" :
+                    String.Format("Name LIKE '%{0}%' OR City LIKE '%{0}%'", txtSearch.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+
+        private void txtSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Enter go to search
+            if (e.KeyChar == 13) btnSearch_Click(null, null);
+        }
+    }
+}
